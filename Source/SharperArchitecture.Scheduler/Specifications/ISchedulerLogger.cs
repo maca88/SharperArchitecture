99d2c81353bb4d4e294e4fba0dc1371b759486d9
maca88 <bostjan.markezic@siol.net>
2017-04-26 19:53:21 +0200

    Removed scheduler project

--- Source/SharperArchitecture.Scheduler/Specifications/ISchedulerLogger.cs
+++
@@ -1,9 +0,0 @@
-﻿using Quartz;
-
-namespace BAF.Scheduler.Specifications
-{
-    public interface ISchedulerLogger : ISchedulerListener, IJobListener, ITriggerListener, ILogRepository
-    {
-        string SchedulerName { get; }
-    }
-}
