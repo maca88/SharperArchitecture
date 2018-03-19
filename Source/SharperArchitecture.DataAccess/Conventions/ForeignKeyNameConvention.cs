e382eae45989622e6870ba38f91c45f7bd329fc8
maca88 <bostjan.markezic@siol.net>
2018-03-19 20:44:51 +0100

    Shorten names for foreign keys

--- Source/SharperArchitecture.DataAccess/Conventions/ForeignKeyNameConvention.cs
+++ Source/SharperArchitecture.DataAccess/Conventions/ForeignKeyNameConvention.cs
@@ -8,19 +8,17 @@ namespace SharperArchitecture.DataAccess.Conventions
     {
         public void Apply(IManyToOneInstance instance)
         {
-            instance.ForeignKey(string.Format("FK_{0}To{1}_{2}", instance.EntityType.Name, instance.Class.Name, instance.Name));
+            instance.ForeignKey($"FK_{instance.EntityType.Name}_{instance.Name}");
         }
 
         public void Apply(IOneToOneInstance instance)
         {
-            var oneToOne = instance as IOneToOneInspector;
-            instance.ForeignKey(string.Format("FK_{0}To{1}_{2}", instance.EntityType.Name, oneToOne.Class.Name, instance.Name));
+            instance.ForeignKey($"FK_{instance.EntityType.Name}_{instance.Name}");
         }
 
         public void Apply(IManyToManyCollectionInstance instance)
         {
-            instance.Relationship.ForeignKey(string.Format("FK_{0}{1}_{2}", instance.EntityType.Name, instance.OtherSide.EntityType.Name,
-                ((ICollectionInspector)instance).Name));
+            instance.Relationship.ForeignKey($"FK_{instance.EntityType.Name}_{((ICollectionInspector) instance).Name}");
         }
     }
 }
